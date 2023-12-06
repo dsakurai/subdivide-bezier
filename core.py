efc917a24319b2aca6707ce0cd9726546f7af130
Daisuke Sakurai <d.sakurai0@gmail.com>
2023-12-06 11:51:36 +0900

    Remove header comments
    Those were left-overs from the IDE default template.

--- core.py
+++ core.py
@@ -1,6 +1,3 @@
-# This is a sample Python script.
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from math import log10
 import numpy as np
 import pandas as pd
